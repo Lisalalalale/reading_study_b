85304c6c8e77ec3361035f9a6f45bbc35536e3c8
Lisalalalale <62019136+Lisalalalale@users.noreply.github.com>
2023-02-22 21:30:25 +0100

    Added a finished button

--- scriptText.js
+++ scriptText.js
@@ -4,13 +4,16 @@ const textSize = urlParams.get('textSize');
 console.log(textSize)
 const body = document.body;
 const trainText = document.getElementById('texttrains')
+const stopButton = document.getElementById('stop-time-button')
 
-
+stopButton.addEventListener("click", stopTimer);
 
 
 //timer methods
 let startTime, endTime, elapsedTime;
 let timerInterval;
+const timer = document.createElement("p");
+const size = document.createElement("p");
 
 function startTimer() {
 // Store the start time
@@ -23,20 +26,26 @@ timerInterval = setInterval(updateTimer, 1000);
 function stopTimer() {
 // Stop the timer
 clearInterval(timerInterval);
-}
-
-function updateTimer() {
-// Calculate the elapsed time
-endTime = new Date();
-elapsedTime = Math.floor((endTime - startTime) / 1000);
-
+//hides the train texts
+trainText.remove();
+// displays the time and the text size
+timer.id="timer";
+body.appendChild(timer);
+body.appendChild(size);
+size.textContent=textSize;
 // Format the time as HH:MM:SS
 const hours = Math.floor(elapsedTime / 3600);
 const minutes = Math.floor((elapsedTime % 3600) / 60);
 const seconds = elapsedTime % 60;
 const formattedTime = `${pad(hours)}:${pad(minutes)}:${pad(seconds)}`;
 
-document.getElementById("timer").innerHTML = formattedTime;
+document.getElementById("timer").innerHTML = formattedTime
+}
+
+function updateTimer() {
+// Calculate the elapsed time
+endTime = new Date();
+elapsedTime = Math.floor((endTime - startTime) / 1000);
 
 
 }
@@ -47,9 +56,6 @@ return value < 10 ? `0${value}` : value;
 }
 
 trainText.style.fontSize = textSize + 'px';
-const timer = document.createElement("p");
-timer.id="timer";
-body.appendChild(timer);
 startTimer();
 
 
